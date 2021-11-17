585e59917fc27122699ced612a9292772720b75b
MatheusRocha <matheus.rocha@corebiz.ag>
2021-11-17 09:59:01 -0300

    init

---
+++ react/buttonBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './style.global.css'
+
+const buttonBar = () => {
+    return (
+        <div className='wrapperButton'>
+            <button className='barbutton'></button>
+        </div>
+    )
+}
+
+export default buttonBar
